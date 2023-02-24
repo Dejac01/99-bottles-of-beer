97769db6b71baea66ef0ed26b9745d1141d3d060
Deja Craft <122240194+Dejac01@users.noreply.github.com>
2023-02-24 14:03:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//for loop
+
+let bottles; 
+
+
+for (num=99;num >= 1; num=num-1){ 
+  if (num==1) {
+   bottles="bottles";
+    
+  } else {
+    bottles="bottles";
+  } 
+  console.log(num+' ' + 'bottles of beer on the wall.');
+  if(num<100) {
+    console.log(num+ ' ' + bottles + ' ' + 'of beer. Take one down pass it around.');
+  }
+}
+
+//While loop
+
+//let x = 3;
+// while x < 35:
+//console.log
+
+//declarative
